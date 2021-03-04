0c81e86a9f164b3a1f7189ca12a5e07029eb1af3
Soojung Kwak <gsj2464@gmail.com>
2021-03-04 20:55:10 +0900

    modified bottom banner

--- script.js
+++ script.js
@@ -99,13 +99,18 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName('bottom-slide');
+
   var dots = document.getElementsByClassName('dot');
   if (n > slides.length) {
     slideIndex = 1;
   }
   if (n < 1) {
+    slideIndex = 1;
+  }
+  if (n > slides.length) {
     slideIndex = slides.length;
   }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
   }
