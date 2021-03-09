aa0eb309763200947c12ce6d03a3b0aad666a49d
Soojung Kwak <gsj2464@gmail.com>
2021-03-09 18:53:03 +0900

    added bottom accordion and modified javascript

---
+++ bottomScript.js
@@ -0,0 +1,72 @@
+// Bottom Banner
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('bottom-slide');
+
+  var dots = document.getElementsByClassName('dot');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = 1;
+  }
+  if (n > slides.length) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+}
+
+// Accordion
+function toggleTopAccordion() {
+  var acc = document.getElementsByClassName('accordion');
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function () {
+      this.classList.toggle('toggled');
+
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + 'px';
+      }
+    });
+  }
+}
+
+function toggleBottomAccordion() {
+  var acc = document.getElementsByClassName('accordion-info')[0];
+  var i;
+
+  acc.addEventListener('click', function () {
+    this.classList.toggle('toggle');
+
+    var panel = document.getElementsByClassName('rp-footer-detail')[0];
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  });
+}
